30cf50f3674cd8ab82b090c7644d1e5a0203e83c
Michał Kozłowski <michalkozlowski@Michals-MacBook-Air-2.local>
2019-08-12 13:24:38 +0200

    block out

--- src/artistsList.js
+++ src/artistsList.js
@@ -34,7 +34,6 @@ class ArtistsList extends Component {
     render() {
         return (
             <div className="workspace">
-                <div className="block"></div>
             <div className="artists-wrapper">
                     {artistsArr.map((el) =>
                         <Artist name={el.name} description={el.description} video={el.video} photo={el.photo}/>
